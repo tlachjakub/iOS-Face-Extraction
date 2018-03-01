d2c548cbf29b1011bbc3e51af63da30d88ba9cc1
Jakub Tlach <tlach.jakub@gmail.com>
2018-03-01 15:17:29 +0900

    Ver. 1.0

---
+++ iOS-Face-Extraction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Face-Extraction
+//
+//  Created by Jakub Tlach on 3/1/18.
+//  Copyright © 2018 Jakub Tlach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
